657332b67ee5ea3cd44214729e3277b6457ecc6b
pkkan <kandrupr@msu.edu>
2018-03-21 22:07:12 -0400

    React and directory structure

---
+++ js/layout.jsx
@@ -0,0 +1,13 @@
+class Layout extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div className="full">Hello</div>
+        );
+    }
+}
+
+ReactDOM.render(<Layout/>, document.getElementById("entry"));
