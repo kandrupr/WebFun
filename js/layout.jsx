97fb60e3a0cf289bfb5f3837ba32aa38734a1c86
pkkan <kandrupr@msu.edu>
2018-03-22 04:33:34 -0400

    Bugs with Responsiveness.

--- js/layout.jsx
+++ js/layout.jsx
@@ -7,13 +7,11 @@ class Layout extends React.Component {
         return (
             <div className="full">
                 <div id="nav_bar"></div>
-                <div className="page">
+                <div className="page first">
                     <div className="pageHalf left r"></div>
                     <div className="pageHalf right b"></div>
                 </div>
-                <div className="page">
-                    <div className="pageHalf left g"></div>
-                    <div className="pageHalf right y"></div>
+                <div className="page g">
                 </div>
             </div>
         );
