b1f48bd09c7af1e263c2f30fedc0a687c13834d0
pkkan <pk.kandru@gmail.com>
2018-04-28 21:25:42 -0400

    Created functions for layers

--- js/anime.js
+++ js/anime.js
@@ -1,112 +1,52 @@
-anime({
-    targets: 'div.b.r',
-    translateX: [
-        { value: 75, duration: 1200 }
-    ], translateY:[
-        { value: 75, duration: 1200 }
-    ], opacity: [
-        {value: 0.0, duration: 800, delay: 1200, easing: 'easeOutExpo'},
-        {value: 1.0, duration: 800, easing: 'easeOutExpo'}
-    ],
-    backgroundColor: '#000',
-    duration: 2800
-});
+var timeline = anime.timeline();
 
-anime({
-    targets: 'div.t.l',
-    translateX: [
-        { value: -75, duration: 1200 }
-    ], translateY:[
-        { value: -75, duration: 1200 }
-    ], opacity: [
-        {value: 0.0, duration: 800, delay: 1200, easing: 'easeOutExpo'},
-        {value: 1.0, duration: 800, easing: 'easeOutExpo'}
-    ],
-    backgroundColor: '#000',
-    duration: 2800
-});
+function addSplit(id, x, y) {
+    timeline.add({
+        targets: id,
+        translateX: [
+            { value: x, duration: 1200 }
+        ], translateY:[
+            { value: y, duration: 1200 }
+        ],
+        backgroundColor: '#000',
+        duration: 1500,
+        delay: 500,
+        offset: 0
+    })
+}
+function addblockRotation(id, distance) {
+    timeline.add({
+        targets: id,
+        rotateX: 60,
+        rotateZ: 45,
+        translateZ: [
+            { value: distance, duration: 300, delay: 1000, easing: 'easeInSine'}
+        ],
+        easing: 'easeInSine',
+        backgroundColor: '#000',
+        duration: 300,
+        offset: 2000
+    });
+}
 
-anime({
-    targets: 'div.t.r',
-    translateX: [
-        { value: 75, duration: 1200 }
-    ], translateY:[
-        { value: -75, duration: 1200 }
-    ], opacity: [
-        {value: 0.0, duration: 800, delay: 1200, easing: 'easeOutExpo'},
-        {value: 1.0, duration: 800, easing: 'easeOutExpo'}
-    ],
-    backgroundColor: '#000',
-    duration: 2800
-});
+addSplit('div.b.r', 75, 75);
+addSplit('div.t.l', -75, -75);
+addSplit('div.t.r', 75, -75);
+addSplit('div.b.l', -75, 75);
 
-anime({
-    targets: 'div.b.l',
-    translateX: [
-        { value: -75, duration: 1200 }
-    ], translateY:[
-        { value: 75, duration: 1200 }
-    ], opacity: [
-        {value: 0.0, duration: 800, delay: 1200, easing: 'easeOutExpo'},
-        {value: 1.0, duration: 800, easing: 'easeOutExpo'}
-    ],
-    backgroundColor: '#000',
-    duration: 2800
-});
-
-anime({
+timeline.add({
     targets: 'div.holder',
     rotateX: 60,
     rotateZ: 45,
-    delay: 2800,
+    backgroundColor: '#000',
     easing: 'easeInSine',
-    duration: 300
+    duration: 300,
+    offset: 2000
 });
 
-anime({
-    targets: 'div.holder.ta',
-    rotateX: 60,
-    rotateZ: 45,
-    translateZ: [
-        { value: -125, duration: 500, delay: 3300, easing: 'easeInSine'}
-    ],
-    easing: 'easeInSine',
-    delay: 2800,
-    duration: 300
-});
+addblockRotation('div.holder.ta', -123);
+addblockRotation('div.holder.ba', 123);
+addblockRotation('div.holder.tb', -246);
+addblockRotation('div.holder.bb', 246);
 
-anime({
-    targets: 'div.holder.ba',
-    rotateX: 60,
-    rotateZ: 45,
-    translateZ: [
-        { value: 125, duration: 500, delay: 3300, easing: 'easeInSine'}
-    ],
-    easing: 'easeInSine',
-    delay: 2800,
-    duration: 300
-});
-
-anime({
-    targets: 'div.holder.tb',
-    rotateX: 60,
-    rotateZ: 45,
-    translateZ: [
-        { value: -250, duration: 500, delay: 3300, easing: 'easeInSine'},
-    ],
-    easing: 'easeInSine',
-    delay: 2800,
-    duration: 300
-});
-
-anime({
-    targets: 'div.holder.bb',
-    rotateX: 60,
-    rotateZ: 45,
-    translateZ: [
-        { value: 250, duration: 500, delay: 3300, easing: 'easeInSine'}
-    ],
-    easing: 'easeInSine',
-    delay: 2800,
-    duration: 300
-});
+timeline.play();
