4eaabf1b61ad836a1e98839f1d8007acd229f649
pkkan <pk.kandru@gmail.com>
2018-05-10 15:19:47 -0400

    Made lines fit to page and scaled. Still high CPU.

--- js/anime.js
+++ js/anime.js
@@ -8,24 +8,69 @@ function addSplit(id, x, y) {
         ], translateY:[
             { value: y, duration: 1200 }
         ],
-        backgroundColor: '#000',
         duration: 1500,
         delay: 500,
         offset: 0
     })
 }
+
 function addblockRotation(id, distance) {
     timeline.add({
         targets: id,
         rotateX: 60,
         rotateZ: 45,
         translateZ: [
-            { value: distance, duration: 300, delay: 1000, easing: 'easeInSine'}
+            { value: distance, duration: 300, delay: 1000, easing: 'easeInSine'},
+        ],
+        easing: 'easeInSine',
+        backgroundColor: "#000",
+        duration: 300,
+        offset: 2000,
+        complete: function(anim) {
+           $(id).children().each(function () {
+               $(this).css("background-color", "transparent");
+           });
+            $(id).css("background-image", "url(../assets/icons/dog.jpg)");
+            $(id).css("background-size", "contain");
+        }
+    });
+}
+
+function selectPage(id, distance, xScale, yScale) {
+    timeline.add({
+        targets: id,
+        rotateX: [
+            {value: 60, duration: 300, easing: 'easeInSine'},
+            {value: 0, duration: 300, delay: 1300, easing: 'easeInSine'}
+        ],
+        rotateZ: [
+            {value: 45, duration: 300, easing: 'easeInSine'},
+            {value: 0, duration: 300, delay: 1300, easing: 'easeInSine'}
+        ],
+        translateZ: [
+            { value: distance, duration: 300, delay: 1000, easing: 'easeInSine'},
+        ],
+        scaleX: [
+            {value: xScale, delay: 1600}
+        ],
+        scaleY: [
+            {value: yScale, delay: 1600}
         ],
         easing: 'easeInSine',
         backgroundColor: '#000',
         duration: 300,
-        offset: 2000
+        offset: 2000,
+        update: function(anim) {
+            if(anim.currentTime > 2600 && anim.currentTime < 2650){
+                $(id).children().each(function () {
+                    $(this).css("background-color", "transparent");
+                });
+                $(id).siblings().each(function () {
+                    $(this).remove();
+                });
+            }
+            console.log(anim.currentTime + 'ms');
+        }
     });
 }
 
@@ -34,19 +79,25 @@ addSplit('div.t.l', -75, -75);
 addSplit('div.t.r', 75, -75);
 addSplit('div.b.l', -75, 75);
 
+selectPage('div.holder.c', 0, getXScale(), getYScale());
+addblockRotation('#center', 0);
+addblockRotation('#mtop', 123);
+addblockRotation('#mbot', -123);
+addblockRotation('#top', 246);
+addblockRotation('#bot', -246);
+
 timeline.add({
-    targets: 'div.holder',
-    rotateX: 60,
-    rotateZ: 45,
-    backgroundColor: '#000',
-    easing: 'easeInSine',
-    duration: 300,
-    offset: 2000
+    targets: '#anim',
+    offset: 3500,
+    complete: function(anim) {
+        var all = document.getElementById("anim");
+    }
 });
+function getXScale() {
+    return Math.round(screen.width/250);
+}
 
-addblockRotation('div.holder.ta', -123);
-addblockRotation('div.holder.ba', 123);
-addblockRotation('div.holder.tb', -246);
-addblockRotation('div.holder.bb', 246);
-
+function getYScale() {
+    return Math.round(screen.height/250);
+}
 timeline.play();
